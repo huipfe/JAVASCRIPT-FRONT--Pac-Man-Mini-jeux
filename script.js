3c80f15f48cb870f63353e781875848c592050cb
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-03-14 10:27:37 +0100

    2e commit PacMan Deplacement

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /** Faire un PACMAN
- * 1) Créer un plateau, dynamiquement.
- * 2) Créer son PACMAN
- * 3) Gérer les déplacement. (sans contrainte)
- * 4) Gérer les contraintes de déplacement (pas dans les murs)
+ * OK 1) Créer un plateau, dynamiquement. 
+ * OK 2) Créer son PACMAN 
+ * OK 3) Gérer les déplacement. (sans contrainte) 
+ * OK 4) Gérer les contraintes de déplacement (pas dans les murs) 
  * 5) Pièce à manger
  * 6) Gérer les fantomes
- * 7)..
+ * 7) Sprite ? Mettre un vrai pacman à la place du point jaune
  */
 
 const gameDiv = document.getElementById("game")
-
+const sizeCaseWitdth = 28
 const layout = [
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -51,11 +51,16 @@ const layout = [
 
 createLayout();
 
+document.addEventListener("keydown", (event) => {
+        deplacerPacMan(event.key); 
+}); 
+
 function createLayout() {
+    let cptCase = 0
     layout.forEach(caseLayout => {
     let casePlateau = document.createElement("div"); 
-    casePlateau.innerHTML = caseLayout;
-
+    casePlateau.dataset.numerocase = cptCase;
+    // .innerHTML = caseLayout;
     // Si case layout vaut 1, alors
     switch(caseLayout){
         case 0 :
@@ -75,7 +80,62 @@ function createLayout() {
             
             break;
 
-    }
+        }
     gameDiv.appendChild(casePlateau);
-});
+    cptCase++;
+
+    });
+    // let caseDepart = document.querySelector("[data-numeroCase='"+489+"']");
+    getCaseByIndex(489).classList.add("pacman");
+}
+
+function getCaseByIndex(index){
+    let caseGame = document.querySelector("[data-numeroCase='" + index + "']");
+    return caseGame;
+}
+
+function deplacerPacMan(direction) {
+    let pacmanDiv = document.querySelector(".pacman")
+    let pacManCase = pacmanDiv.dataset.numerocase;
+    let caseDestination = null
+    switch (direction) {
+        case "ArrowUp":
+            // Déplacer la case contenant pac-man, de 1 vers le haut
+            caseDestination = getCaseByIndex(parseInt(pacManCase) - sizeCaseWitdth)
+            break;
+        case "ArrowRight":
+            // Déplacer la case contenant pac-man, de 1 vers la droite
+            caseDestination = getCaseByIndex(parseInt(pacManCase) + 1)
+
+            break;
+        case "ArrowLeft":
+            // Déplacer la case contenant pac-man, de 1 vers la gauche
+            caseDestination = getCaseByIndex(parseInt(pacManCase) - 1 )
+
+            break;
+        case "ArrowDown":
+            // Déplacer la case contenant pac-man, de 1 vers le bas
+            caseDestination = getCaseByIndex(parseInt(pacManCase) + sizeCaseWitdth)          
+            break;
+        default:
+            break;
+    };
+    if (caseDestination != null) {
+        if (checkDirection(caseDestination)) {
+            pacmanDiv.classList.remove("pacman");
+            caseDestination.classList.add("pacman")
+        }
+    }
+}
+
+// Return faux si je peux pas aller la ou je veux
+// return vrai si je peux
+function checkDirection (caseDestination)
+{
+    if (caseDestination.classList.contains("mur")) {
+        return false;
+    }
+    else {
+        return true; 
+    }
 }
